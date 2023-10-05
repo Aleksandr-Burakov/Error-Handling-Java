55ce2bc1a9d4ce184e54f7227e3d45d41ef195b4
Aleksandr Burakov <aleks.bur.git@gmail.com>
2023-10-06 01:56:24 +0300

    homework02 task01

---
+++ homeworks/homework02/task01/InputPrint.java
@@ -0,0 +1,34 @@
+package homevorks.homework02.task01;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class InputPrint {
+    
+    public static void printUser(String Message) {
+        System.out.println("Вы ввели требуемое значение - " + Message);
+    }
+
+    public static void printError(String errorMessage) {
+        System.err.println("Error: " + errorMessage);
+    }
+
+    public static Float inputUser(){
+        float number;
+        System.out.println("Введите дробное число разделенное '.' (точкой): ");
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+        try {
+            number = scanner.nextFloat();    
+        } catch (Exception e) {
+            printError("Введено некоректное значение: "); 
+            number = inputUser();  
+        } 
+        scanner.close();
+        return number;  
+    }
+  
+    public static void main(String[] args){
+        Float meaning = inputUser();
+        printUser("Число: " + meaning);
+    }
+}
